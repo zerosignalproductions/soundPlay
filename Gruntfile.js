7f842feeec77e5db0f2e171c1d63ac61e55fbfbb
Justin Myers <justin@zerosignalproductions.com>
2014-03-12 01:47:39 -0500

    Updated package.json

--- Gruntfile.js
+++ Gruntfile.js
@@ -45,9 +45,6 @@ module.exports = function ( grunt ) {
         '/**\n' +
         ' * <%= pkg.name %> - v<%= pkg.version %> - <%= grunt.template.today("yyyy-mm-dd") %>\n' +
         ' * <%= pkg.homepage %>\n' +
-        ' *\n' +
-        ' * Copyright (c) <%= grunt.template.today("yyyy") %> <%= pkg.author %>\n' +
-        ' * Licensed <%= pkg.licenses.type %> <<%= pkg.licenses.url %>>\n' +
         ' */\n'
     },
 
